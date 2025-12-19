e05f0c10f49b6bbe87220f2ad16a5bbe40a57a5d
anushkaa-23 <agarwal.anushka0823@gmail.com>
2025-12-19 17:24:31 +0530

    statistics_practice.py

---
+++ statistics_practice.py
@@ -0,0 +1,11 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# 1. Random data generate karo jo Normal Distribution follow kare
+data = np.random.normal(loc=0, scale=1, size=1000) 
+
+# 2. Isse visualize karo (Seaborn use karke)
+sns.histplot(data, kde=True)
+plt.title("Mera Pehla Gaussian Distribution")
+plt.show()
