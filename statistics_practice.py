286dc9e42d73a830a9ea5861bbc416deba78fe67
anushkaa-23 <agarwal.anushka0823@gmail.com>
2025-12-19 17:29:50 +0530

    statistics_practice.py

--- statistics_practice.py
+++ statistics_practice.py
@@ -2,10 +2,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# 1. Random data generate karo jo Normal Distribution follow kare
+# 1. Random data generate
 data = np.random.normal(loc=0, scale=1, size=1000) 
-
-# 2. Isse visualize karo (Seaborn use karke)
 sns.histplot(data, kde=True)
 plt.title("Gaussian Distribution")
 plt.show()
